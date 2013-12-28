38c715bd52b046696efbc092976f6e0d27cd6771
Daniel Kertesz <daniel@spatof.org>
2013-12-28 17:04:52 +0100

    API to toggle following status

--- qstode/views/api.py
+++ qstode/views/api.py
@@ -10,6 +10,8 @@
 """
 from flask import jsonify, request
 from flask.views import MethodView
+from flask.ext.login import current_user
+from sqlalchemy import and_
 from qstode.app import app
 from qstode import db
 from qstode import model
@@ -91,3 +93,49 @@ def complete_tags():
                         for tag in tags])
 
     return jsonify(results=results)
+
+
+@app.route('/api/is_following/<int:user_id>')
+def is_following(user_id):
+    """JSON: check following status of the specified user"""
+
+    if not current_user.is_authenticated() or user_id == current_user.id:
+        return jsonify(result=2)
+
+    wu = model.watched_users
+
+    other_user = model.User.query.get_or_404(user_id)
+    result = db.Session.query(wu).\
+             filter(and_(wu.c.user_id == current_user.id,
+                         wu.c.other_user_id == other_user.id)).\
+             count()
+    
+    print "Result: {}".format(result)
+    if result > 0:
+        return jsonify(result=1)
+    else:
+        return jsonify(result=0)
+
+
+@app.route('/api/follow/<int:user_id>')
+def follow_user(user_id):
+    """Toggle following status of the specified user"""
+
+    if not current_user.is_authenticated() or user_id == current_user.id:
+        return jsonify(result=2, msg="Invalid request")
+
+    wu = model.watched_users
+    other_user = model.User.query.get_or_404(user_id)
+    query = db.Session.query(wu).\
+             filter(and_(wu.c.user_id == current_user.id,
+                         wu.c.other_user_id == other_user.id))
+
+    if query.count() == 0:
+        current_user.watched_users.append(other_user)
+        result = 1
+    else:
+        current_user.watched_users.remove(other_user)
+        result = 0
+
+    db.Session.commit()
+    return jsonify(result=result)

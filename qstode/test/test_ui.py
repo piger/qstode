05ce05150cf1a59d31f86d98e15887ef955b1621
Daniel Kertesz <dkertesz@zendesk.com>
2017-10-27 18:19:47 +0100

    retry tests

--- qstode/test/test_ui.py
+++ qstode/test/test_ui.py
@@ -1,6 +1,11 @@
+import os
+import tempfile
+import shutil
+from flask_testing import LiveServerTestCase
 import pytest
 from selenium.webdriver.common.keys import Keys
-from qstode.test import FlaskTestCase
+from qstode import main
+from qstode import db
 
 
 @pytest.fixture
@@ -11,11 +16,40 @@ def chrome_options(chrome_options):
 
 
 @pytest.mark.usefixtures('selenium')
-class IntegrationTestCase(FlaskTestCase):
+class IntegrationTestCase(LiveServerTestCase):
     def __init__(self, *args, **kwargs):
         super(IntegrationTestCase, self).__init__(*args, **kwargs)
         self.s = None
-        self.base_url = 'http://127.0.0.1:5000'
+
+    def create_app(self):
+        self.tmp_dir = tempfile.mkdtemp()
+        self.db_filename = os.path.join(self.tmp_dir, "db.sqlite")
+
+        config = {
+            # I hate the author of Flask-WTF for changing the name of the
+            # configuration parameter.
+            'WTF_CSRF_ENABLED': False,
+            'CSRF_ENABLED': False,
+            'SQLALCHEMY_DATABASE_URI': "sqlite:///%s" % self.db_filename,
+            'SECRET_KEY': 'test',
+            'TESTING': True,
+            'LIVESERVER_PORT': 5123,
+        }
+
+        return main.create_app(config)
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.Session.remove()
+        db.drop_all()
+        shutil.rmtree(self.tmp_dir)
+
+    def _load_data(self, data):
+        for item in data:
+            db.Session.add(item)
+        db.Session.commit()
 
     @pytest.fixture(autouse=True)
     def add_selenium(self, selenium):
@@ -23,7 +57,7 @@ class IntegrationTestCase(FlaskTestCase):
         self.s = selenium
 
     def get(self, url_tail):
-        return self.s.get(self.base_url + url_tail)
+        return self.s.get(self.get_server_url() + url_tail)
 
     def test_home(self):
         self.get('/')

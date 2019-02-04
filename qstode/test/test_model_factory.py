52aa8c13b12e214fa646b5278c1547420a4c9be6
Daniel Kertesz <daniel@spatof.org>
2019-02-04 19:05:09 +0000

    test strategy

--- qstode/test/test_model_factory.py
+++ qstode/test/test_model_factory.py
@@ -14,7 +14,8 @@ class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     _profile = factory.Faker('profile')
 
     username = factory.LazyAttribute(lambda obj: obj._profile["username"])
-    email = factory.LazyAttribute(lambda obj: obj._profile["mail"])
+    # if we get a unique username, our email will be unique as well
+    email = factory.LazyAttribute(lambda obj: "{.username}@example.com".format(obj))
     display_name = factory.LazyAttribute(lambda obj: obj._profile["name"])
     password = factory.Faker('password')
     active = True
@@ -31,6 +32,7 @@ class TagFactory(factory.alchemy.SQLAlchemyModelFactory):
     name = factory.Faker("word")
 
 
+# TODO: this can lead to duplicates
 class LinkFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = Link
@@ -51,11 +53,11 @@ class BookmarkFactory(factory.alchemy.SQLAlchemyModelFactory):
     @classmethod
     def _create(cls, model_class, *args, **kwargs):
         link = LinkFactory()
-        tags = TagFactory.create_batch(3)
+        # tags = TagFactory.create_batch(3)
 
         obj = model_class(*args, **kwargs)
         obj.link = link
-        obj.tags = tags
+        # obj.tags = tags
         db.Session.commit()
         return obj
 
@@ -66,6 +68,17 @@ class BookmarkFactory(factory.alchemy.SQLAlchemyModelFactory):
 
         if extracted:
             self.user = extracted
+
+    @factory.post_generation
+    def tags(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for tag in extracted:
+                # qui devo CREARE dei tag object!
+                # oppure potrei passare degli oggetti Tag...
+                self.tags.append(Tag.get_or_create(tag))
 
 
 class FactoryModelTest(FlaskTestCase):    
@@ -92,3 +105,31 @@ class FactoryModelTest(FlaskTestCase):
 
         result = Bookmark.by_user(user.id)
         assert result.count() == 10
+
+    def test_search_by_tags(self):
+        """Test for retrieving bookmarks by tags
+
+        1) Create 3 bookmarks
+        2) tag 2 bookmarks with "web"
+        3) tag 1 bookmark with "search"
+        4) count
+        """
+
+        user1 = UserFactory(username="pippo")
+        user2 = UserFactory(username="gladiolo")
+        db.Session.commit()
+
+        tags1 = ['web', '2.0']
+        tags2 = ['search', 'apocalypse']
+
+        # TODO: qui sarebbe figo poter passare delle tag
+        user1_bks = BookmarkFactory.create_batch(2, user=user1, tags=tags1)
+        user2_bks = BookmarkFactory.create_batch(1, user=user2, tags=tags2)
+        db.Session.commit()
+
+        for tags, count in ((['web'], 2), (['search'], 1)):
+            result = Bookmark.by_tags(tags)
+            assert result.count() == count
+
+        assert Bookmark.by_user(user1.id).count() == 2
+        assert Bookmark.by_user(user2.id).count() == 1
